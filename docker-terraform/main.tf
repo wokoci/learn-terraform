d56c4a9c0bb6f23382e6d0e118bb7eb91791b63a
Ikpeazu Wokocha <wokoci@gmail.com>
2022-06-21 19:15:59 +0100

    add output with join

--- docker-terraform/main.tf
+++ docker-terraform/main.tf
@@ -20,5 +20,15 @@ resource "docker_container" "docker_nodered_image" {
     internal = 1880
     external = 1880
   }
+}
 
+output "container-name" {
+  description = "Output aboput docjer container"
+  value       = docker_container.docker_nodered_image.name
+}
+
+
+output "ip-address" {
+  description = "details about external ip address to running node-red "
+  value       = join(":", [docker_container.docker_nodered_image.ip_address, docker_container.docker_nodered_image.ports[0].external])
 }
