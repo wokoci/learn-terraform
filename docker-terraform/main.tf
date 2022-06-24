cef542beed09b113811f162de2034204ca49b03f
Ikpeazu Wokocha <wokoci@gmail.com>
2022-06-24 07:12:06 +0100

    learned about loops

--- docker-terraform/main.tf
+++ docker-terraform/main.tf
@@ -14,28 +14,15 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
+  count   = 2
   length  = 4
   upper   = false
   special = false
 }
 
-resource "random_string" "random1" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
 resource "docker_container" "docker_nodered_image" {
-  name  = "nodered"
-  image = docker_image.nodered_image.latest
-  ports {
-    internal = 1880
-    #external = 1880
-  }
-}
-
-resource "docker_container" "docker_nodered_image1" {
-  name  = join("nodered1", [random_string.random.result])
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
@@ -44,22 +31,12 @@ resource "docker_container" "docker_nodered_image1" {
 }
 
 output "container-name" {
-  description = "Output aboput docker container"
-  value       = docker_container.docker_nodered_image.name
-}
-
-output "container-name1" {
   description = "output about container 1"
-  value       = docker_container.docker_nodered_image1.name
+  value       = docker_container.docker_nodered_image[*].name
 }
 
 
 output "ip-address" {
   description = "details about external ip address to running node-red "
-  value       = join(":", [docker_container.docker_nodered_image.ip_address, docker_container.docker_nodered_image.ports[0].external])
-}
-
-output "ip-address1" {
-  description = "IP address details for cotainer 1"
-  value       = join(":", [docker_container.docker_nodered_image1.ip_address, docker_container.docker_nodered_image1.ports[0].external])
+  value       =  [for i in docker_container.docker_nodered_image[*]: join(":",[i.ip_address],[i.ports[0].external])]
 }
