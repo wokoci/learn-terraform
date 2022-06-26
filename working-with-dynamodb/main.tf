0ec217919956ed005825a9fb31d46d7539b42e5b
Ikpeazu Wokocha <wokoci@gmail.com>
2022-06-26 17:31:48 +0100

    add dynamodn example

---
+++ working-with-dynamodb/main.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "cars" {
+  name         = "cars"
+  hash_key     = "VIN"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "VIN"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "car-iterms" {
+  table_name = aws_dynamodb_table.cars.name
+  hash_key   = aws_dynamodb_table.cars.hash_key
+  item       = <<-EOF
+  {
+    "Manufacturer": {"S":"Toyota"},
+    "Make": {"S": "Corolla"},
+    "Year": {"N":"2004"},
+    "VIN" : {"S": "4Y1SL65848Z411439"}
+  },
+
+  EOF
+}
