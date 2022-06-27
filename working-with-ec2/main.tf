4b9b690e22d260ec67bf2353d71048426741d3bc
Ikpeazu Wokocha <wokoci@gmail.com>
2022-06-27 17:32:47 +0100

    add sg

--- working-with-ec2/main.tf
+++ working-with-ec2/main.tf
@@ -17,8 +17,24 @@ resource "aws_instance" "webServer" {
                 EOF
 
     key_name = aws_key_pair.webKey.id
+    vpc_security_group_ids = [aws_security_group.ssh_access.id]
 }
 
 resource "aws_key_pair" "webKey" {
   public_key = file("${path.root}/.ssh/myKey.cer")
 }
+
+resource "aws_security_group" "ssh_access" {
+  name ="ssh acces"
+  description = "Sg to allow access from internet"
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "value"
+    from_port = 22
+    to_port=22
+    protocol = "tpc"
+    security_groups = [ "value" ]
+  } ]
+
+}
+
