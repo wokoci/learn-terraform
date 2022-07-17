98607692c61f367e9af3c279cc39c87bea7f523e
Ikpeazu Wokocha <wokoci@gmail.com>
2022-07-17 14:39:23 +0100

    add terraform.tfvars file and maintf files

--- provision-ec2/provider.tf
+++ provision-ec2/provider.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_providers{
-    aws ={
-        source = "hashicorp/aws"
-        version = "4.15.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.15.0"
     }
   }
 }
---
+++ provision-ec2/terraform.tfvars
@@ -0,0 +1,5 @@
+variable "region" {
+  type        = string
+  description = "The region to be used for the setup"
+  default     = "us-east-1"
+}
