bcdd27abe7eece5913c228f89ca1c6ae98d594cf
Ikpeazu Wokocha <wokoci@gmail.com>
2022-07-23 21:11:50 +0100

    add key resouce

--- provision-ec2/main.tf
+++ provision-ec2/main.tf
@@ -2,4 +2,15 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
   ami           = "ami-0cff7528ff583bf9a"
 
+  tags = {
+    key   = "Name"
+    value = "Nginx web server"
+  }
 }
+
+
+resource "aws_key_pair" "cerberus-key" {
+    key_name = "cerberus"
+    public_key = file(".ssh/cerberus.pub")
+}
+
